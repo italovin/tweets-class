00bf87e4e5934346c946e582144ea299f6a95e84
italovin <hitalomacedo900@gmail.com>
2024-02-20 17:05:31 -0300

    fix: Button color parsing

--- Features/Questions/Helpers/LabelParser.cs
+++ Features/Questions/Helpers/LabelParser.cs
@@ -1,14 +1,19 @@
 using blazor_test.Features.Questions;
 
+public class LabelInfo {
+    public string Text { get; set; } = "Indefinido";
+    public string RelatedColor { get; set; } = "#000000";
+}
+
 public static class LabelParser{
 
-    public static string ParseLabel(LabelEnum labelEnum){
+    public static LabelInfo ParseLabel(LabelEnum labelEnum){
         return labelEnum switch
         {
-            LabelEnum.Negative => "Negativo",
-            LabelEnum.Neutral => "Neutro",
-            LabelEnum.Positive => "Positivo",
-            _ => "Indefinido"
+            LabelEnum.Negative => new LabelInfo { Text = "Negativo", RelatedColor = "#ff0000"},
+            LabelEnum.Neutral => new LabelInfo { Text = "Neutro", RelatedColor = "#808080"},
+            LabelEnum.Positive => new LabelInfo { Text = "Positivo", RelatedColor = "#00ff00"},
+            _ => new LabelInfo { Text = "Indefinido", RelatedColor = "#000000"}
         };
     }
 
