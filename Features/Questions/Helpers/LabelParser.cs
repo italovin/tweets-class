761dabb90c04348e339d00edadb0e2359ed4816e
italovin <hitalomacedo900@gmail.com>
2024-02-21 14:21:19 -0300

    Change questions button colors to component class

--- Features/Questions/Helpers/LabelParser.cs
+++ Features/Questions/Helpers/LabelParser.cs
@@ -2,7 +2,7 @@ using blazor_test.Features.Questions;
 
 public class LabelInfo {
     public string Text { get; set; } = "Indefinido";
-    public string RelatedColor { get; set; } = "#000000";
+    public string RelatedButtonStyle { get; set; } = "btn";
 }
 
 public static class LabelParser{
@@ -10,10 +10,10 @@ public static class LabelParser{
     public static LabelInfo ParseLabel(LabelEnum labelEnum){
         return labelEnum switch
         {
-            LabelEnum.Negative => new LabelInfo { Text = "Negativo", RelatedColor = "#ff0000"},
-            LabelEnum.Neutral => new LabelInfo { Text = "Neutro", RelatedColor = "#808080"},
-            LabelEnum.Positive => new LabelInfo { Text = "Positivo", RelatedColor = "#00ff00"},
-            _ => new LabelInfo { Text = "Indefinido", RelatedColor = "#000000"}
+            LabelEnum.Negative => new LabelInfo { Text = "Negativo", RelatedButtonStyle = "btn btn-danger"},
+            LabelEnum.Neutral => new LabelInfo { Text = "Neutro", RelatedButtonStyle = "btn btn-secondary"},
+            LabelEnum.Positive => new LabelInfo { Text = "Positivo", RelatedButtonStyle = "btn btn-success"},
+            _ => new LabelInfo { Text = "Indefinido", RelatedButtonStyle = "btn"}
         };
     }
 
